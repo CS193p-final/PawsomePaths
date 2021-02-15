036864fb9c68155ed0726ea9384241740b9602f0
DT Pham <dt99.pham@gmail.com>
2021-02-14 16:16:31 -0800

    Initial Commit

---
+++ Hex/HexApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexApp.swift
+//  Hex
+//
+//  Created by Duong Pham on 2/14/21.
+//
+
+import SwiftUI
+
+@main
+struct HexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
