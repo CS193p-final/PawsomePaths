33d3e64a832c61ec57305a76e878f90cd10960dc
DT Pham <dt99.pham@gmail.com>
2021-02-15 17:06:59 -0800

    Create the skeleton for Monte Carlo tree search.

---
+++ Hex/AI/MonteCarlo.swift
@@ -0,0 +1,26 @@
+//
+//  MonteCarlo.swift
+//  Hex
+//
+//  Created by Duong Pham on 2/15/21.
+//
+
+import Foundation
+
+struct MonteCarlo {
+    var board: GameBoard
+    
+    func update() {
+        
+    }
+    
+    // Causes the AI to calculate the best move from the current game state and return it
+    func getPlay() {
+        
+    }
+    
+    // Plays out a "random" game from current position, then updates the statistics tables with the result.s
+    func runSimulation() {
+        
+    }
+}
