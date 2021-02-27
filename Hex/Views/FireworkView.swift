8c2c0fb989b38e2a18ab046317763fd5ac9f1a3f
Giang Nguyen <gn02.nguyen@gmail.com>
2021-02-26 16:58:27 -0800

    idk what happened but I think I saw the firework now, really tiny but I saw it!!!

--- Hex/Views/FireworkView.swift
+++ Hex/Views/FireworkView.swift
@@ -10,13 +10,41 @@ import SwiftUI
 
 struct FireworkRepresentable: UIViewRepresentable {
     let fireworkController = ClassicFireworkController()
-    let label = UILabel()
+    let label = UIButton()
     func makeUIView(context: Context) -> some UIView {
-        label.text = "🎉🎉🎉"
+        label.setTitle("🎉🎉🎉", for: .normal)
+        label.style()
         return label
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
-        fireworkController.addFireworks(sparks: 2, around: UILabel())
+        fireworkController.addFireworks(sparks: 2, around: label)
+    }
+}
+
+final class FireworkView: UIViewController {
+    let fireworkController = ClassicFireworkController()
+}
+
+extension UIButton {
+
+    func style() {
+        let color = UIColor(red:0.42, green:0.58, blue:0.98, alpha:1.00)
+        self.backgroundColor = color
+        self.setTitleColor(.white, for: .normal)
+        self.layer.cornerRadius = 8
+        self.titleLabel!.font = UIFont.boldSystemFont(ofSize: 20)
+
+        let layer = self.layer
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 10)
+        layer.shadowRadius = 6.0
+        layer.shadowOpacity = 0.3
+
+        let shadowWidth = layer.bounds.width * 0.9
+        let shadowRect = CGRect(x: 0 + (layer.bounds.width - shadowWidth) / 2.0, y: 0, width: shadowWidth, height: layer.bounds.height)
+        layer.shadowPath = UIBezierPath(rect: shadowRect).cgPath
+
+        layer.zPosition = 2
     }
 }
