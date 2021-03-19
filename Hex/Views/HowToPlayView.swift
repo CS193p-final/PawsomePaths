96eb082c7f6efd911f3b5b1feb59da7b81454295
Giang Nguyen <gn02.nguyen@gmail.com>
2021-03-19 12:11:52 -0700

    Initial commit for launch screen

--- Hex/Views/HowToPlayView.swift
+++ Hex/Views/HowToPlayView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct HowToPlayView: View {
     @State private var welcomeView = false
     @State var soundOn: Bool
-    let red = Color(red: 0.9296875, green: 0.46, blue: 0.453)
-    let blue = Color(red:0.39, green:0.55, blue:0.894)
-    let fontSize: CGFloat = 15
-    let hPadding: CGFloat = 10
-    let vPadding: CGFloat = 5
-
+    private let red = Color(red: 0.9296875, green: 0.46, blue: 0.453)
+    private let blue = Color(red:0.39, green:0.55, blue:0.894)
+    private let fontSize: CGFloat = 45
+    private let hPadding: CGFloat = 10
+    private let vPadding: CGFloat = 5
+    private let hunterGreen = Color(red: 0.15625, green: 0.3125, blue: 0.1796875, opacity: 0.5)
+    
     var body: some View {
         if welcomeView {
             WelcomeView()
@@ -24,21 +25,21 @@ struct HowToPlayView: View {
                 VStack {
                     Text("Back")
                         .padding()
-                        .foregroundColor(.blue)
-                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundColor(hunterGreen)
+                        .frame(maxWidth: .infinity, alignment: .leading).font(Font.custom("PressStart2P-Regular", size: geometry.size.width / fontSize))
                         .onTapGesture {
                             welcomeView = true
                             playSound("MouseClick", type: "mp3", soundOn: soundOn)
                         }
                     Text("Connect top and bottom side of the board to win")
-                        .font(Font.custom("KronaOne-Regular", size: fontSize))
+                        .font(Font.custom("KronaOne-Regular", size: geometry.size.width / fontSize))
                         .foregroundColor(red)
                         .padding(.horizontal, hPadding)
                         .padding(.vertical, vPadding)
                         .frame(maxWidth: .infinity, alignment: .leading)
 
                     Text("Connect left and right side of the board to win")
-                        .font(Font.custom("KronaOne-Regular", size: fontSize))
+                        .font(Font.custom("KronaOne-Regular", size: geometry.size.width / fontSize))
                         .foregroundColor(blue)
                         .padding(.horizontal, hPadding)
                         .padding(.vertical, vPadding)
