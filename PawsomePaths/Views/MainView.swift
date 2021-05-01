fe5e03c14ee9cf7a4e38222eb4f181f49894f67a
DT Pham <dt99.pham@gmail.com>
2021-04-30 21:50:39 -0700

    Revert changes that crash play online mode.

--- PawsomePaths/Views/MainView.swift
+++ PawsomePaths/Views/MainView.swift
@@ -10,31 +10,18 @@ import SwiftUI
 struct MainView: View {
     @EnvironmentObject var viewRouter: ViewRouter
     @EnvironmentObject var audioManager: AudioManager
-    @EnvironmentObject var modalManager: ModalManager
-
+    
     var body: some View {
         switch viewRouter.currentScreen {
         case .welcome:
             WelcomeView()
-                .environmentObject(viewRouter)
-                .environmentObject(audioManager)
-                .environmentObject(modalManager)
         case .singlePlayerGame(let continueGame):
             GameView(hexGame: SinglePlayerGame(name: "singlePlayer"), continueGame: continueGame)
-                .environmentObject(viewRouter)
-                .environmentObject(audioManager)
-                .environmentObject(modalManager)
         case .twoPlayersGame(let continueGame):
             GameView(hexGame: TwoPlayersGame(name: "twoPlayer"), continueGame: continueGame)
-                .environmentObject(viewRouter)
-                .environmentObject(audioManager)
-                .environmentObject(modalManager)
         case .onlineGame:
             let onlineGame: OnlineGame? = OnlineGame()
             OnlineGameView(hexGame: onlineGame!)
-                .environmentObject(viewRouter)
-                .environmentObject(audioManager)
-                .environmentObject(modalManager)
         case .howToPlay:
             HowToPlayView(soundOn: true)
         case .friendList:
