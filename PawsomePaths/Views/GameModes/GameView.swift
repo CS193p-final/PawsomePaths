21964c397ffc10ae745215431ed2a1ec4e4e5f3f
Giang Nguyen <gn02.nguyen@gmail.com>
2021-05-03 00:51:07 -0700

    Remove new game button struct inside game view + change opacity of new game button when game end.

--- PawsomePaths/Views/GameModes/GameView.swift
+++ PawsomePaths/Views/GameModes/GameView.swift
@@ -119,37 +119,10 @@ struct GameView: View {
                     .foregroundColor(!showResult ? .blue : .gray)
                     .padding()
                     .zIndex(-1)
+                    .opacity(showResult ? 0.5 : 1)
                 }
                 ModalAnchorView().environmentObject(modalManager)
             }
-        }
-    }
-}
-
-
-struct newGameButton: View {
-    @EnvironmentObject var viewRouter: ViewRouter
-
-    var isOnlineGame: Bool
-    var game: GameMode
-    let buttonFontSize: CGFloat
-    var showResult: Bool
-    private let hunterGreen = Color(red: 0.15625, green: 0.3125, blue: 0.1796875, opacity: 0.5)
-    
-    var body: some View {
-        Button(action: {
-            if isOnlineGame {
-                viewRouter.currentScreen = .onlineGame
-            } else {
-                showResult ? game.newGame(size: game.board.size) : nil
-            }
-        }
-    ) {
-            RoundedRectangle(cornerRadius: buttonFontSize)
-                .frame(width: buttonFontSize * 10, height: buttonFontSize * 3, alignment: .center)
-                .overlay(Text("Reset Game").foregroundColor(.white)
-                            .font(Font.custom("KronaOne-Regular", size: buttonFontSize)))
-                .foregroundColor(hunterGreen)
         }
     }
 }
